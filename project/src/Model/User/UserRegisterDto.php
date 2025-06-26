e395fa1aa87167343d2113ea0288d34221e1a47b
novapc74 <novapc74@yandex.ru>
2025-06-26 20:53:36 +0300

    add confirm password to userRegisterDto

--- project/src/Model/User/UserRegisterDto.php
+++ project/src/Model/User/UserRegisterDto.php
@@ -3,17 +3,31 @@
 namespace App\Model\User;
 
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 readonly class UserRegisterDto
 {
     public function __construct(
         #[Assert\NotBlank(message: 'Почта - обязательное поле.')]
+        #[Assert\Email(message: 'Формат строки - Email.')]
         public string $email,
 
         #[Assert\NotBlank(message: 'Пароль - обязательное поле.')]
         public string $password,
+
+        #[Assert\NotBlank(message: 'Подтвердить пароль - обязательное поле.')]
+        public string $confirm_password,
     )
     {
     }
 
+    #[Assert\Callback]
+    public static function validate(mixed $value, ExecutionContextInterface $context, mixed $payload): void
+    {
+        if ($value->password !== $value->confirm_password) {
+            $context->buildViolation('Пароли должны совпадать.')
+                ->atPath('confirm_password')
+                ->addViolation();
+        }
+    }
 }
