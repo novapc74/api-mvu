f9ac20a651f5767b6a8c2ebcb935ac4eb8001512
novapc74 <novapc74@yandex.ru>
2025-06-29 08:25:46 +0300

    require twig & make login form

--- project/src/Controller/SecurityController.php
+++ project/src/Controller/SecurityController.php
@@ -2,45 +2,32 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Exception\CustomException;
-use App\Model\User\UserRegisterDto;
-use App\Service\Security\SecurityService;
-use Symfony\Component\HttpFoundation\Response;
+use LogicException;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Security\Http\Attribute\CurrentUser;
-use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-#[Route('/api/auth')]
-final class SecurityController extends AbstractController
+class SecurityController extends AbstractController
 {
-    public function __construct(private readonly SecurityService $service)
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-    }
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
 
-    #[Route('/login', name: 'api_login', methods: ['POST'])]
-    public function login(#[CurrentUser] ?User $user): Response
-    {
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
 
-        if (null === $user) {
-            return new JsonResponse([
-                'message' => 'missing credentials',
-            ], Response::HTTP_UNAUTHORIZED);
-        }
-        return $this->json([
-            'username' => $user->getUserIdentifier(),
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
         ]);
     }
 
-    /**
-     * @throws CustomException
-     */
-    #[Route('/register', methods: ['POST'])]
-    public function register(#[MapRequestPayload] UserRegisterDto $dto): JsonResponse
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
     {
-        return new JsonResponse($this->service->register($dto), Response::HTTP_CREATED);
+        throw new LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
