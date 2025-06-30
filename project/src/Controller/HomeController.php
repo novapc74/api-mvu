093faee02b463df6c639fb870856be308a12f6f5
novapc74 <novapc74@yandex.ru>
2025-06-30 19:23:47 +0300

    add array_rund() in home page

--- project/src/Controller/HomeController.php
+++ project/src/Controller/HomeController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $haystack = ['info', 'success', 'error', 'warning'];
+        $randomKey = array_rand($haystack);
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'type' => $haystack[$randomKey],
         ]);
     }
 }
