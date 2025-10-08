be09e63e4171365892e9f623387fd4a7079d5aa7
novapc74 <novapc74@yandex.ru>
2025-10-08 16:51:23 +0300

    update product fixture class

--- project/src/DataFixtures/ProductFixtures.php
+++ project/src/DataFixtures/ProductFixtures.php
@@ -4,9 +4,9 @@ namespace App\DataFixtures;
 
 use App\Entity\Product;
 use App\Entity\Category;
-use App\Entity\ProductProperty;
 use App\Entity\Property;
 use ReflectionException;
+use App\Entity\ProductProperty;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
@@ -16,6 +16,10 @@ class ProductFixtures extends AppFixtures implements DependentFixtureInterface
         '95', '5', '180'
     ];
 
+    private const THIRD_LEVEL_CATEGORIES_COUNT = 26;
+    private const PRODUCT_COUNT = 324;
+    private const PROPERTY_COUNT = 3;
+
     /**
      * @throws ReflectionException
      */
@@ -23,43 +27,40 @@ class ProductFixtures extends AppFixtures implements DependentFixtureInterface
     {
         $thirdLevelCategories = array_map(
             fn(int $i) => $this->getReference("ThirdLevel_Category_$i", Category::class),
-            range(0, 26)
+            range(0, self::THIRD_LEVEL_CATEGORIES_COUNT)
         );
 
-
         /** Для каждой категории создаём 12 товаров */
-        $this->createEntity(Product::class, 324, function (Product $product, int $count) use ($thirdLevelCategories) {
-            $index = intdiv($count - 1, 12);
+        $this->createEntity(Product::class, self::PRODUCT_COUNT, function (Product $product, int $count) use ($thirdLevelCategories) {
 
+            $index = intdiv($count - 1, 12);
             $name = mb_ucfirst(self::getName($index));
 
             $product
-                ->setName(self::generateName("{$name}_", $count));
-
-            $product->setCategory($thirdLevelCategories[$index]);
+                ->setName(self::generateName("{$name}_", $count))
+                ->setCategory($thirdLevelCategories[$index]);
         });
 
         $products = [];
-        for ($i = 0; $i < 324; $i++) {
+        for ($i = 0; $i < self::PRODUCT_COUNT; $i++) {
             $products[] = $this->getReference("Product_$i", Product::class);
         }
 
         $properties = [];
-        for ($i = 0; $i < 3; $i++) {
+        for ($i = 0; $i < self::PROPERTY_COUNT; $i++) {
             $properties[] = $this->getReference("Property_$i", Property::class);
         }
 
-        // Создаем ProductProperty для каждого продукта
-        // Для каждого продукта выбираем 3-5 случайных свойств
+        /** Создаем ProductProperty для каждого продукта */
         foreach ($products as $product) {
 
             foreach ($properties as $key => $property) {
 
-                $productProperty = new ProductProperty();
-                $productProperty->setProduct($product);
-                $productProperty->setProperty($property);
-                $productProperty->setScale($key); // Случайный порядок сортировки (scale)
-                $productProperty->setValue(self::PROPERTY_VALUES[$key]); // Случайное значение (адаптируйте под тип свойства, если нужно)
+                $productProperty = (new ProductProperty())
+                    ->setProduct($product)
+                    ->setProperty($property)
+                    ->setScale($key)
+                    ->setValue(self::PROPERTY_VALUES[$key]);
 
                 $manager->persist($productProperty);
             }
